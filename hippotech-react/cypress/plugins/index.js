879ef66bce8d107825f0f3a9698877da66b84530
Dylan <dylan.c.moses@gmail.com>
2023-12-21 13:16:47 -0500

    updating cypress

--- hippotech-react/cypress/plugins/index.js
+++ hippotech-react/cypress/plugins/index.js
@@ -17,6 +17,8 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('before:browser:launch', (browser, launchOptions) => {
+    launchOptions.args.push('--disable-gpu');
+    launchOptions.args.push('--disable-software-rasterizer');
+    return launchOptions;});
 }
