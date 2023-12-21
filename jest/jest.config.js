644724de783372207e990a436a0611364cd66a56
Dylan <dylan.c.moses@gmail.com>
2023-12-21 12:24:19 -0500

    removing cypress testing

--- jest/jest.config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  "setupFiles": ["<rootDir>/helpers.js"],
-  "reporters": ["default", "jest-junit"],
-  "testTimeout": 15000
-};
-
-module.exports = config;
--- jest/login.test.js
+++
@@ -1,6 +0,0 @@
-
-describe('Login', () => {
-  it('is possible', async () => {
-    const token = await getTokenAsync();
-  })
-})
